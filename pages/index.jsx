459134ef5acca5f68981a668e11ffc16044d48f6
tayloraustinwebb-debug <tayloraustinwebb@gmail.com>
2026-03-23 10:43:23 -0700

    Update and rename index.tsx to index.jsx

--- pages/index.jsx
+++ pages/index.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function Home() {
   return (
-    <div style={{padding: 40, fontFamily: "Arial"}}>
+    <div style={{ padding: 40, fontFamily: "Arial" }}>
       <h1>Paint Pals Website</h1>
       <p>Your site is successfully deployed. Next step: paste your full design code here.</p>
     </div>
