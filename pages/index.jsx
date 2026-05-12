59360f648a9ecbd436b4a87a92c6a9696ec96dec
tayloraustinwebb-debug <tayloraustinwebb@gmail.com>
2026-05-11 22:34:20 -0700

    Update index.jsx

--- pages/index.jsx
+++ pages/index.jsx
@@ -674,6 +674,7 @@ container.addEventListener("click", handleClick);
           </div>
         </section>
       </div>
+           </div>
 
         <section className="mx-auto max-w-6xl px-4 py-14">
   <div className="flex flex-col gap-5">
